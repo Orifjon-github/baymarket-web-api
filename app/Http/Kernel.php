fbc5db68da42ff9f2f15dd9da794e913b3a09da1
Orifjon94 <avazbekoripov94@gmail.com>
2023-03-13 21:33:15 +0500

    Cors Allow

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            Cors::class
         ],
 
         'api' => [
@@ -69,6 +70,6 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-//        'cors' => Cors::class
+        'cors' => Cors::class
     ];
 }
--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -7,7 +7,7 @@ class Cors
     {
         $response = $next($request);
 
-        $response->header('Access-Control-Allow-Origin: https://wemardpanel.netlify.app')
+        $response->header('Access-Control-Allow-Origin', '*')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 
         if ($request->getMethod() === 'OPTIONS') {
