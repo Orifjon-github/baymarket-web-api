858f0f5c05439a7bd0a1ea2a6f74ff1b3ef74890
Orifjon94 <avazbekoripov94@gmail.com>
2023-03-13 20:44:01 +0500

    Cors Allow

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -5,10 +5,31 @@ class Cors
 {
     public function handle($request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization')
-            ->header('Access-Control-Allow-Credentials',' true');
+        $response = $next($request);
+
+        $response->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+        if ($request->getMethod() === 'OPTIONS') {
+            $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'))
+                ->header('Access-Control-Max-Age', '86400');
+        }
+
+        if ($response->status() === 500) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Internal server error'
+            ]);
+        }
+
+        if ($response->status() === 404) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Resource not found'
+            ]);
+        }
+
+        return $response;
     }
+
 }
