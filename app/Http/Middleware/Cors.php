41a99913400cb779e9c80efdf37746fbdb116507
Orifjon94 <avazbekoripov94@gmail.com>
2023-03-13 21:30:45 +0500

    Cors Allow

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -7,7 +7,7 @@ class Cors
     {
         $response = $next($request);
 
-        $response->header('Access-Control-Allow-Origin', '*')
+        $response->header('Access-Control-Allow-Origin: https://wemardpanel.netlify.app')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 
         if ($request->getMethod() === 'OPTIONS') {
