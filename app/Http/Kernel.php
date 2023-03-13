c895559dafdc2493b7851ad9d257b2c091a0e96a
Orifjon94 <avazbekoripov94@gmail.com>
2023-03-13 21:40:23 +0500

    Cors Allow

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Http;
 
-use App\Http\Middleware\Cors;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -22,7 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-//        \App\Http\Middleware\Cors::class,
+        \App\Http\Middleware\Cors::class,
 //        \Fruitcake\Cors\HandleCors::class,
 
     ];
@@ -40,7 +39,6 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            Cors::class
         ],
 
         'api' => [
@@ -48,7 +46,6 @@ class Kernel extends HttpKernel
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
 //             \Fruitcake\Cors\HandleCors::class,
-        Cors::class
         ],
     ];
 
@@ -70,6 +67,5 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'cors' => Cors::class
     ];
 }
--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,34 +1,19 @@
 <?php
+
 namespace App\Http\Middleware;
+
 use Closure;
+
 class Cors
 {
     public function handle($request, Closure $next)
     {
         $response = $next($request);
 
-        $response->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-
-        if ($request->getMethod() === 'OPTIONS') {
-            $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
-        }
-
-//        if ($response->status() === 500) {
-//            return response()->json([
-//                'status' => 'error',
-//                'message' => 'Internal server error'
-//            ]);
-//        }
-
-        if ($response->status() === 404) {
-            return response()->json([
-                'status' => 'error',
-                'message' => 'Resource not found'
-            ]);
-        }
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type');
 
         return $response;
     }
-
 }
